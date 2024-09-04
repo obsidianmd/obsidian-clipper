983cff76732764afc008fca27daf8817322fcb5d
Steph Ango <10565871+kepano@users.noreply.github.com>
2024-09-04 13:08:32 -0700

    Resolve icons import

--- src/icons.d.ts
+++ src/icons.d.ts
@@ -1,6 +1,4 @@
-declare module './icons' {
-	import { IconNode } from 'lucide';
-	
-	export const icons: Record<string, IconNode>;
-	export function initializeIcons(root?: Document): void;
-}
+import { IconNode } from 'lucide';
+
+export const icons: Record<string, IconNode>;
+export function initializeIcons(root?: HTMLElement | Document): void;
