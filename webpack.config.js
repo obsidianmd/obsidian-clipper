85554bcf254055cfa55605319abb814016b03e74
Steph Ango <10565871+kepano@users.noreply.github.com>
2024-09-11 12:24:08 -0700

    Source maps

--- webpack.config.js
+++ webpack.config.js
@@ -41,8 +41,18 @@ module.exports = (env, argv) => {
 					test: /\.scss$/,
 					use: [
 						MiniCssExtractPlugin.loader,
-						'css-loader',
-						'sass-loader'
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+							}
+						}
 					]
 				}
 			]
