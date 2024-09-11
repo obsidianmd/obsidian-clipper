3288ac84d6cadd403552f6c2ac07e1d28199d9a8
Steph Ango <10565871+kepano@users.noreply.github.com>
2024-09-11 11:07:14 -0700

    More config stuff

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build for Chrome
-echo "Building for Chrome..."
-cp src/manifest.chrome.json dist/manifest.json
-
-# Build for Firefox
-echo "Building for Firefox..."
-cp src/manifest.firefox.json dist_firefox/manifest.json
