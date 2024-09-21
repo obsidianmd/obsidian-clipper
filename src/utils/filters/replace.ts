275f1a3643f43da1e798dfdd76bfecf9ab227896
Steph Ango <10565871+kepano@users.noreply.github.com>
2024-09-21 08:16:40 -0700

    More clarifications

--- src/utils/filters/replace.ts
+++ src/utils/filters/replace.ts
@@ -10,11 +10,23 @@ export const replace = (str: string, param?: string): string => {
 	const replacements = param.split(/,(?=(?:[^"]*"[^"]*")*[^"]*$)/);
 
 	return replacements.reduce((acc, replacement) => {
-		const [search, replace] = replacement.split(':').map(p => {
+		let [search, replace] = replacement.split(/(?<!\\):/).map(p => {
 			// Remove surrounding quotes and unescape characters
 			return p.trim().replace(/^["']|["']$/g, '').replace(/\\(.)/g, '$1');
 		});
+
 		// Use an empty string if replace is undefined or an empty string
-		return acc.replace(new RegExp(search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g'), replace || '');
+		replace = replace || '';
+
+		// For | and : characters, use string.split and join for replacement
+		if (search === '|' || search === ':') {
+			return acc.split(search).join(replace);
+		}
+
+		// Escape special regex characters in search string
+		const searchRegex = new RegExp(search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g');
+
+		// Use a custom replace function to handle global replacement
+		return acc.replace(searchRegex, replace);
 	}, str);
 };
