e637f93e437f8531a25bd91c4ca413808eb16481
Steph Ango <10565871+kepano@users.noreply.github.com>
2024-09-24 20:50:34 -0700

    Add legacy mode

--- src/utils/obsidian-note-creator.ts
+++ src/utils/obsidian-note-creator.ts
@@ -106,7 +106,7 @@ export async function saveToObsidian(
 		// Use clipboard
 		navigator.clipboard.writeText(fileContent).then(() => {
 			obsidianUrl += `&clipboard`;
-			obsidianUrl += `&content=${encodeURIComponent("Web Clipper requires Obsidian 1.7.2 or above. You may need to install the [early access](https://help.obsidian.md/Obsidian/Early+access+versions) version.")}`;
+			obsidianUrl += `&content=${encodeURIComponent("Web Clipper requires Obsidian 1.7.2 or above. You may need to install the [early access](https://help.obsidian.md/Obsidian/Early+access+versions) version, or enable legacy mode in Web Clipper settings.")}`;
 			openObsidianUrl(obsidianUrl);
 		}).catch(err => {
 			console.error('Failed to copy content to clipboard:', err);
