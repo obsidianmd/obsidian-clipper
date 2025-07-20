9f9df55a83e309858c4e5a9e7bdbd380b79cb71e
Elliott <elliottjprice@outlook.com>
2025-07-20 16:54:08 +1000

    Fixed clipper not correctly choosing src for images with srcset

--- src/utils/markdown-converter.ts
+++ src/utils/markdown-converter.ts
@@ -143,12 +143,41 @@ export function createMarkdownContent(content: string, url: string) {
 		replacement: function(content, node) {
 			const figure = node as HTMLElement;
 			const img = figure.querySelector('img');
+			console.log(img)
 			const figcaption = figure.querySelector('figcaption');
 			
 			if (!img) return content;
 
 			const alt = img.getAttribute('alt') || '';
-			const src = img.getAttribute('src') || '';
+			let src = img.getAttribute('src') || '';
+			const srcset = img.getAttribute('srcset') || '';
+
+			if (srcset) {
+
+				let images: {src: string, width: number}[] = [];
+
+				// deconstruct the srcset urls
+				const srcsetUrls = srcset.split(",%20");
+				for (let url of srcsetUrls) {
+					
+					const split = url.split("%20");
+					const srcUrl = split[0];
+					const width = parseInt(split[1]);
+
+					images.push({src: srcUrl, width});
+				}
+
+				// we need to choose one of the images to use from the srcset
+				// for convenience, simply grab the first one
+				// this could be updated to take width into account
+
+				src = images[0].src;
+			}
+
+			
+
+
+			console.log({alt, src, srcset})
 			let caption = '';
 
 			if (figcaption) {
@@ -638,7 +667,10 @@ export function createMarkdownContent(content: string, url: string) {
 	}
 
 	try {
+		console.log(processedContent)
+		
 		let markdown = turndownService.turndown(processedContent);
+		console.log(markdown)
 		debugLog('Markdown', 'Markdown conversion successful');
 
 		// Remove the title from the beginning of the content if it exists
@@ -664,6 +696,7 @@ export function createMarkdownContent(content: string, url: string) {
 		
 		// Clear the footnotes object for the next conversion
 		Object.keys(footnotes).forEach(key => delete footnotes[key]);
+		console.log(markdown)
 
 		return markdown.trim();
 	} catch (error) {
