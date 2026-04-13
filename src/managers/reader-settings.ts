0eb23d42f65cce0270188d697377cf53483f9f3f
kepano <10565871+kepano@users.noreply.github.com>
2026-04-13 16:44:45 -0700

    Remove option to follow links

--- src/managers/reader-settings.ts
+++ src/managers/reader-settings.ts
@@ -378,10 +378,6 @@ export async function initializeReaderSettings() {
 		updatePreview();
 	});
 
-	initializeSettingToggle('reader-follow-links', generalSettings.readerSettings.followLinks, (checked) => {
-		saveSettings({ ...generalSettings, readerSettings: { ...generalSettings.readerSettings, followLinks: checked } });
-	});
-
 	const themeModeSelect = document.getElementById('reader-appearance') as HTMLSelectElement;
 	if (themeModeSelect) {
 		themeModeSelect.value = generalSettings.readerSettings.appearance;
